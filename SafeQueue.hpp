7f23e8e63630ebb3c1f3b643cc08b54df2f33bb3
ketchuupp <a.chrobak23@gmail.com>
2022-04-28 11:47:59 +0200

    Create SafeQueue

---
+++ SafeQueue.hpp
@@ -0,0 +1,44 @@
+//
+// Created by arek on 26.04.2022.
+//
+
+#ifndef CPP_CPU_TRUCKER_SAFEQUEUE_HPP
+#define CPP_CPU_TRUCKER_SAFEQUEUE_HPP
+
+#include <queue>
+#include <mutex>
+#include <utility>
+#include <iostream>
+
+template<class T>
+class SafeQueue {
+    std::queue<T> s_queue;
+    std::mutex queue_mutex;
+public:
+    SafeQueue() {}
+
+    void push(const T &value) {
+        std::lock_guard<std::mutex> guard(queue_mutex);
+        s_queue.push(value);
+    }
+
+    void push(T &&value) {
+        std::lock_guard<std::mutex> guard(queue_mutex);
+        s_queue.push(value);
+    }
+
+    T pop() {
+        std::lock_guard<std::mutex> guard(queue_mutex);
+        while (s_queue.empty()) {} // wait for element
+
+        T value = s_queue.front();
+        s_queue.pop();
+        return value;
+    }
+
+    size_t get_size() { return s_queue.size(); }
+
+};
+
+
+#endif //CPP_CPU_TRUCKER_SAFEQUEUE_HPP
