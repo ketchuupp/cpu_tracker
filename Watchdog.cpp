df17d7ba7336c5c3288ef783df11098b7db8b71d
ketchuupp <a.chrobak23@gmail.com>
2022-04-28 12:04:00 +0200

    Add Watchdog class

---
+++ Watchdog.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arek on 28.04.2022.
+//
+
+#include "Watchdog.h"
---
+++ Watchdog.h
@@ -0,0 +1,14 @@
+//
+// Created by arek on 28.04.2022.
+//
+
+#ifndef CPP_CPU_TRUCKER_WATCHDOG_H
+#define CPP_CPU_TRUCKER_WATCHDOG_H
+
+
+class Watchdog {
+
+};
+
+
+#endif //CPP_CPU_TRUCKER_WATCHDOG_H
